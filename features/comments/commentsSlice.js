b7cb32d4a7fa5766612d8d22f7d81685973c0bbd
cecilia <cecilia1262@hotmail.com>
2024-09-09 20:07:42 -0500

    Favorites

--- features/comments/commentsSlice.js
+++ features/comments/commentsSlice.js
@@ -13,25 +13,33 @@ export const fetchComments = createAsyncThunk(
   }
 );
 
+export const postComment = createAsyncThunk(
+  "comments/postComment",
+  async (payload, { dispatch, getState }) => {
+    setTimeout(() => {
+      const { comments } = getState();
+      payload.date = new Date().toISOString();
+      payload.id = comments.commentsArray.length;
+      dispatch(addComment(payload));
+    }, 2000);
+  }
+);
+
 const commentsSlice = createSlice({
   name: "comments",
-  initialState: { isLoading: true, errMess: null, commentsArray: [] },
-  reducers: {},
+  initialState: { commentsArray: [] },
+  reducers: {
+    addComment: (state, action) => {
+      state.commentsArray.push(action.payload);
+    },
+  },
   extraReducers: (builder) => {
-    builder
-      .addCase(fetchComments.pending, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(fetchComments.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.errMess = null;
-        state.commentsArray = action.payload;
-      })
-      .addCase(fetchComments.rejected, (state, action) => {
-        state.isLoading = false;
-        state.errMess = action.error ? action.error.message : "Fetch failed";
-      });
+    builder.addCase(fetchComments.fulfilled, (state, action) => {
+      state.commentsArray = action.payload;
+    });
   },
 });
 
-export const commentsReducer = commentsSlice.reducer;
+export const { addComment } = commentsSlice.actions;
+
+export default commentsSlice.reducer;
